c87913b9f84db4bf893fd8a1128c33f68936832f
magcurt08 <magcurt08@magcurt08s-MBP.home>
2020-12-05 23:10:57 -0500

    Solves part two of Day 1

--- readFile.ts
+++ readFile.ts
@@ -16,9 +16,12 @@ lineReader.on('line', function (line) {
 lineReader.on('close', function () {
     const sortedArray: number[] = array.sort((n1,n2) => n1 - n2)
 
-    let answer = calculateProduct(sortedArray, 0, sortedArray.length - 1)
+    let partOneAnswer = calculateProduct(sortedArray, 0, sortedArray.length - 1)
 
-    console.log(`Here be the answer: ${answer}`)
+    let middle = Math.floor(sortedArray.length / 2) - 1;
+    let partTwoAnswer = threesACrowd(sortedArray, 0, middle, sortedArray.length - 1)
+
+    console.log(`Here be the answer to Part One: ${partOneAnswer} \nHere be the answer to Part Two: ${partTwoAnswer}`)
 });
 
 
@@ -46,5 +49,28 @@ const calculateProduct = (arr: number[], first: number, last: number) => {
         return calculateProduct(arr, first + 1, last)
     } else {
         return arr[first] * arr[last]
+    }
+}
+
+// I'm probably overdoing something here but I'm drunk so ¯\_(ツ)_/¯
+const threesACrowd = (arr: number[], first: number, middle: number, last: number) => {
+    let sum = arr[first] + arr[middle] + arr[last]
+    let firstValue = arr[first]
+    let middleValue = arr[middle]
+    let lastValue = arr[last]
+    let product = firstValue * middleValue * lastValue
+
+    if (sum > 2020) {
+        if ((2020 - (firstValue + lastValue)) < firstValue) {
+            return threesACrowd(arr, first, middle, last - 1)
+        }
+        if (middleValue + lastValue > 2020) {
+            return threesACrowd(arr, first, middle - 1, last)
+        }
+        if (2020 % (sum) > firstValue) {
+            return threesACrowd(arr, first, middle, last - 1)
+        }
+    } else if (sum === 2020) {
+        return product;
     }
 }
